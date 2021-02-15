6b6d2598710b3e82b08f0301c1224b2ccd7da69b
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-02-15 10:04:17 -0300

    chore: dglearn project created

---
+++ backend/src/main/java/com/devgabriel/dglearn/DglearnApplication.java
@@ -0,0 +1,13 @@
+package com.devgabriel.dglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DglearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DglearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devgabriel/dglearn/DglearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgabriel.dglearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DglearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
