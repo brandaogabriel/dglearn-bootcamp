1989406a2acf750605b8af29a5f7fd867ec51549
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-02-17 15:17:26 -0300

    feat: add Custom content for current user, JPQL query

---
+++ backend/src/main/java/com/devgabriel/dglearn/services/NotificationService.java
@@ -0,0 +1,28 @@
+package com.devgabriel.dglearn.services;
+
+import com.devgabriel.dglearn.dto.NotificationDTO;
+import com.devgabriel.dglearn.entities.Notification;
+import com.devgabriel.dglearn.entities.User;
+import com.devgabriel.dglearn.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private NotificationRepository repository;
+
+	@Autowired
+	private AuthService authService;
+
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
+		User user = authService.authenticated();
+		Page<Notification> page = repository.find(user, unreadOnly, pageable);
+		return page.map(NotificationDTO::new);
+	}
+}
